7bf2644ad0fb43da0ecd6f72b591304af8056608
tarsic99 <tarsic99@gmail.com>
2017-12-27 13:34:23 -0600

    methods like open_home_page moved from tests to login etxc.

--- test_add_group.py
+++ test_add_group.py
@@ -18,20 +18,14 @@ class Test(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="gpojkpkjda", header="ka lcac", footer="lknmclskncs"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -41,6 +35,8 @@ class Test(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
+
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -55,11 +51,14 @@ class Test(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
+
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -69,6 +68,7 @@ class Test(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         # OR wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
 
 
     def open_home_page(self, wd):
